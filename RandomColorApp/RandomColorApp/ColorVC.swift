b9dc1c5cd282833ed872dfa710c96c6b741361e5
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-01 01:21:20 +0300

    tableView

--- RandomColorApp/RandomColorApp/ColorVC.swift
+++ RandomColorApp/RandomColorApp/ColorVC.swift
@@ -2,23 +2,27 @@
 
 import UIKit
 
-class ColorVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ColorVC: UIViewController {
  
     override func viewDidLoad() {
         super.viewDidLoad()
+        
       
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-    
-
-
 
 }
 
+extension ColorVC : UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toDetailVC", sender: self)
+    }
+}
