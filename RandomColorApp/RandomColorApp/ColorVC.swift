7b9020d06f5ec3176843da6191bbb7a3ae401f64
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-01 01:12:10 +0300

    Fixed

--- RandomColorApp/RandomColorApp/ColorVC.swift
+++ RandomColorApp/RandomColorApp/ColorVC.swift
@@ -2,13 +2,22 @@
 
 import UIKit
 
-class ColorVC: UIViewController {
-
+class ColorVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
       
-        
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
 
 
 }
