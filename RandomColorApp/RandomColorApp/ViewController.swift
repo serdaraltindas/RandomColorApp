58e7a37ccf67ce5d0ecd20c89750b2cee7a512e7
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-01 00:18:27 +0300

    File

---
+++ RandomColorApp/RandomColorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomColorApp
+//
+//  Created by Serdar Altındaş on 1.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
