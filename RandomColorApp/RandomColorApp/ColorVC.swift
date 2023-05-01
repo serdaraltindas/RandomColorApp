69809015517625354cd40d06186ad4fe7c1fc08b
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-01 11:49:26 +0300

    Colors Table View

--- RandomColorApp/RandomColorApp/ColorVC.swift
+++ RandomColorApp/RandomColorApp/ColorVC.swift
@@ -4,24 +4,40 @@ import UIKit
 
 class ColorVC: UIViewController {
  
+    var colors : [UIColor] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        addRandomColors()
       
     }
-
+    
+    func addRandomColors() {
+        for _ in 0..<50 {
+            colors.append(createRandomColor())
+        }
+    }
+    
+    func createRandomColor() -> UIColor{
+        let randomColor = UIColor(red: CGFloat.random(in: 0...1),
+                                  green: CGFloat.random(in: 0...1),
+                                  blue: CGFloat.random(in: 0...1),
+                                  alpha: 1)//Görünürlük.
+        return randomColor
+    }
 }
 
 extension ColorVC : UITableViewDelegate, UITableViewDataSource {
-
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return colors.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
     }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "toDetailVC", sender: self)
     }
